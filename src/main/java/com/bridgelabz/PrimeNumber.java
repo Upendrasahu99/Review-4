674bfa787d4a7b82c6e2eb4ebe5d474a494641dd
upendra <upendrasahu1199@gmail.com>
2023-03-06 15:26:12 +0530

    Review 4

--- src/main/java/com/bridgelabz/PrimeNumber.java
+++ src/main/java/com/bridgelabz/PrimeNumber.java
@@ -1,24 +1,42 @@
-//Write a program to find all prime number from an array and sort it before print all prime number.
+//
 
 package com.bridgelabz;
 
+import jdk.jshell.spi.SPIResolutionException;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
+
+import static java.util.Arrays.sort;
 
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] input = new int[10];
-        int count = 0;
+        int[] primeNumberArray = new int[input.length];
+
+
         for (int i = 0; i < input.length; i++) {
-
             input[i] = scanner.nextInt();
+        }
 
-            for (int j = 2; j <= input[i]; j++) {
-                if (input[i] != 0 && input[i] != 1 && input[i] % j != 0) {
-                    count++;
+        int index = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            int count = 0;
+            if (input[i] > 1) {
+                for (int j = 2; j < input[i]; j++) {
+                    if (input[i] % j == 0) {
+                        count++;
+                    }
+                }
+                if (count == 0) {
+                    primeNumberArray[index] = input[i];
+                    index++;
+
                 }
             }
-
         }
 
     }
